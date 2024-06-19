ee38f41da7232d49df631482a9e88adf2d0810ce
Max <schulz.max5@gmail.com>
2024-06-19 16:09:56 +0200

    small bug fix

--- run_WP1.py
+++ run_WP1.py
@@ -2,12 +2,14 @@ from WP1.experiment_logic import WP1Session
 from utils.utils import get_input_from_dict
 
 info = get_input_from_dict({"subject_id": 99,  # enter subject id
-                            "test": 0,  # enter if test run (1) or not (0)
+                            "test": True,  # enter if test run (1) or not (0)
                             "block": 0})
+if info["subject_id"] < 10:
+    info["subject_id"] = f'0{info["subject_id"]}'
 
 # create subject sequence
 # generate_subject_sequence(subject_id=info["subject_id"])
 sess = WP1Session(output_str=f'sub-{info["subject_id"]}', output_dir="WP1/logs", settings_file="WP1/config.yaml",
                   starting_block=info["block"], test=True if info["test"] == 1 else False)
 sess.run()
-sess.quit(test=True if info["test"] == 1 else False)
+sess.quit()
