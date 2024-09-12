5440cd4f731fb2944e79b4ee4e4bae12c158a0a4
Max <schulz.max5@gmail.com>
2024-09-12 14:24:19 +0200

    create all the high pitched targets and stuff

--- SPACEPRIME/create_stimuli.py
+++ SPACEPRIME/create_stimuli.py
@@ -16,39 +16,38 @@ with open(settings_path) as file:
 # load up stimuli
 orig_stims = [slab.Sound.read(os.path.join(stimdir, f"{i}")) for i in os.listdir(stimdir)]
 
-
 # make distractors of low and high saliency
-distractor_low_dir = os.path.join(root, "distractors_low")
 distractor_high_dir = os.path.join(root, "distractors_high")
+
 try:
     os.mkdir(distractor_high_dir)
-    os.mkdir(distractor_high_dir)
 except FileExistsError:
-    print(f"Directory {distractor_low_dir} or {distractor_high_dir} already exists")
+    print(f"stimuli directories exist already!")
 
 # for distractor generation, we need to change the pitch. we do this by increasing and decreasing the pitch of the
 # original stimuli.
-low_pitch_factor = -10
 high_pitch_factor = 10
 for stimname, stim in zip(os.listdir(stimdir), orig_stims):
     digit = stimname.split(".")[0]
-    low_pitched = shift_pitch(stim, pitch_factor=low_pitch_factor)  # low pitched
-    low_pitched.write(os.path.join(distractor_low_dir, digit + f"_low_pitched_factor_{low_pitch_factor}.wav"))
-    high_pitched = shift_pitch(stim, pitch_factor=high_pitch_factor)  # high pitched
-    high_pitched.write(os.path.join(distractor_high_dir, digit + f"_high_pitched_factor_{high_pitch_factor}.wav"))
+    high_pitched_distractor = shift_pitch(stim, pitch_factor=high_pitch_factor)  # high pitched
+    high_pitched_distractor.write(os.path.join(distractor_high_dir, digit + f"_high_pitched_factor_{high_pitch_factor}.wav"))
 
 # make targets
 modulation_freq = 30  # amplitude modulation
 
 # generate target directory
-targetdir = os.path.join(root, f"targets_{modulation_freq}_Hz")
+targetdir_high = os.path.join(root, f"targets_high_{modulation_freq}_Hz")
+targetdir_low = os.path.join(root, f"targets_low_{modulation_freq}_Hz")
 try:
-    os.mkdir(targetdir)
+    os.mkdir(targetdir_high)
+    os.mkdir(targetdir_low)
 except FileExistsError:
-    print(f"Directory {targetdir} already exists")
+    print(f"Directory already exists")
 
 for stimname, stim in zip(os.listdir(stimdir), orig_stims):
     digit = stimname.split(".")[0]
-    target = modulate_amplitude(stim, modulation_freq=modulation_freq)  # low pitched
-    target.write(os.path.join(targetdir, digit + f"_amplitude_modulated_{modulation_freq}.wav"))
-
+    target_low = modulate_amplitude(stim, modulation_freq=modulation_freq)  # low pitched
+    target_low.write(os.path.join(targetdir_low, digit + f"_amplitude_modulated_{modulation_freq}.wav"))
+    target_high = shift_pitch(stim, pitch_factor=high_pitch_factor)
+    target_high = modulate_amplitude(target_high, modulation_freq=modulation_freq)
+    target_high.write(os.path.join(targetdir_high, digit + f"_amplitude_modulated_{modulation_freq}.wav"))
