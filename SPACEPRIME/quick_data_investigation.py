af40c18728daac1e10d9c980c20be9f9a57e9ac2
Max <schulz.max5@gmail.com>
2024-09-20 14:53:23 +0200

    small update

--- SPACEPRIME/quick_data_investigation.py
+++ SPACEPRIME/quick_data_investigation.py
@@ -3,25 +3,34 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 from utils.stats import permutation_test
-import matplotlib
-matplotlib.use("Qt5Agg")
+#import matplotlib
+#matplotlib.use("Qt5Agg")
 plt.ion()
 
 
 # path for figure saving
 figpath = "C:\PycharmProjects\psychopy-experiments\WP1\\figures"
 # load up dataframe
-fp = "C:\\PycharmProjects\\psychopy-experiments\\SPACEPRIME\\results\\"
+fp = "C:\\PycharmProjects\\psychopy-experiments\\SPACEPRIME\\logs\\clean"
 file_excel = os.listdir(fp)[0]
 # read dataframe
-df = pd.read_excel(os.path.join(fp, file_excel), index_col=0)
+df = pd.read_excel(os.path.join(fp, file_excel))
 # get results only
-results = df[(df['event_type'] == 'response') & (df['rt'] != 0)]
+results = df[(df['event_type'] == 'mouse_click') & (df['rt'] != 0) & (df["phase"] == 1)]
+# Create a boolean mask to identify consecutive duplicates in `ITI-Jitter`
+mask = results['ITI-Jitter'] != results['ITI-Jitter'].shift(1)
+
+# Filter the DataFrame to keep only the unique rows
+df_filtered = results[mask].copy()
+
+# Display the shape of the original and filtered DataFrames
+print(f"Original DataFrame shape: {results.shape}")
+print(f"Filtered DataFrame shape: {df_filtered.shape}")
 # singleton absent versus present percentage correct
-plot = sns.barplot(data=results, x="SingletonPresent", y="iscorrect", hue="subject_id")
+plot = sns.barplot(data=df_filtered, x="SingletonPresent", y="iscorrect", hue="block")
 plt.close()
 # singleton absent versus present reaction time
-sns.barplot(data=results, x="SingletonPresent", y="rt", hue="subject_id")
+sns.barplot(data=df_filtered, x="SingletonPresent", y="rt", hue="subject_id")
 plt.close()
 # spatial priming is_correct
 sns.barplot(data=results, y=results.iscorrect, hue=results.SpatialPriming)
