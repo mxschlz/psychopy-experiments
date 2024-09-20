d84ba09cde7d72ef03157a36683f2497f940a844
Max <schulz.max5@gmail.com>
2024-09-20 14:52:29 +0200

    send eeg triggers and set response blox line color to black every trial

--- SPACEPRIME/experiment_logic.py
+++ SPACEPRIME/experiment_logic.py
@@ -3,9 +3,9 @@ import os
 import SPACEPRIME.prompts as prompts
 import pandas as pd
 from utils import set_logging_level
-import numpy as np
-import time
 from psychopy.sound import Sound
+from psychopy import parallel, core
+from SPACEPRIME.encoding import EEG_TRIGGER_MAP, PRIMING
 
 
 class SpaceprimeTrial(Trial):
@@ -25,8 +25,9 @@ class SpaceprimeTrial(Trial):
             self.session.default_fix.draw()
         # play stimulus in phase 0
         if self.phase == 0:
+            self.session.virtual_response_box[0].lineColor = "black"
             self.stim.play()
-            time.sleep(self.stim.duration)
+            core.wait(self.stim.duration)
 
         # get response in phase 1
         if self.phase == 1:
@@ -39,10 +40,12 @@ class SpaceprimeTrial(Trial):
 
         # print too slow warning if response is collected in phase 2
         if self.phase == 2:
-            while self.session.timer.getTime() < 0:
-                if any(self.get_events()) or any(self.session.mouse.getPressed()):
-                    self.session.display_text(text=prompts.too_slow, color=(1.0, 0.0, 0.0),
-                                              duration=np.abs(self.session.timer.getTime()))  # red warning
+            # while self.session.timer.getTime() < 0:
+            if any(self.get_events()) or any(self.session.mouse.getPressed()):
+                #self.session.display_text(text=prompts.too_slow, color=(1.0, 0.0, 0.0),
+                                          #duration=np.abs(self.session.timer.getTime()))  # red warning
+                self.session.virtual_response_box[0].lineColor = "red"
+                # self.session.win.flip()
 
 
 class SpaceprimeSession(Session):
@@ -59,6 +62,7 @@ class SpaceprimeSession(Session):
         self.test = test
         self.this_block = None
         self.subject_id = int(self.output_str.split("-")[1])
+        self.trials = []
         # slab.set_default_level(self.settings["session"]["level"])
         # print(f"Set stimulus level to {slab.sound._default_level}")
 
@@ -76,7 +80,6 @@ class SpaceprimeSession(Session):
         self.sequence = pd.read_excel(self.blockdir + ".xlsx")
 
     def create_trials(self, n_trials, durations, timing='seconds'):
-        self.trials = []
         for trial_nr in range(n_trials):
             trial = SpaceprimeTrial(session=self,
                                     trial_nr=trial_nr,
@@ -86,13 +89,18 @@ class SpaceprimeSession(Session):
                                                     block=self.this_block,
                                                     subject_id=self.subject_id),
                                     verbose=True,
-                                    timing=timing)
+                                    timing=timing,
+                                    draw_each_frame=True)  # TODO: figure out if True or False is more suitable
             sound_path = os.path.join(trial.session.blockdir, f"s_{trial_nr}.wav")  # s_0, s_1, ... .wav
             trial.stim = Sound(sound_path)
             # trial.stim = slab.Binaural.read(sound_path)
             self.trials.append(trial)
 
     def run(self):
+        # TODO: where to set the port address?
+        if self.settings["mode"]["record_eeg"]:
+            parallel.setPortAddress("0xCFF8")  # set address of port
+        self.send_trigger("experiment_onset")
         # welcome the participant
         self.display_text(text=prompts.welcome1, keys="space")
         self.display_text(text=prompts.welcome2, keys="space")
@@ -108,9 +116,15 @@ class SpaceprimeSession(Session):
                                timing=self.settings["session"]["timing"])
             self.start_experiment()
             for trial in self.trials:
+                self.send_trigger("trial_onset")
+                trigger_name = f'Target-{int(trial.parameters["TargetLoc"])}-Singleton-{int(trial.parameters["SingletonLoc"])}-{PRIMING[trial.parameters["Priming"]]}'
+                self.send_trigger(trigger_name)
                 trial.run()
+                self.send_trigger("trial_offset")
         else:
+            self.start_experiment()
             for block in self.blocks:
+                self.send_trigger("block_onset")
                 self.set_block(block=block)
                 self.load_sequence()
                 self.create_trials(n_trials=self.n_trials,
@@ -118,20 +132,39 @@ class SpaceprimeSession(Session):
                                               self.settings["session"]["response_duration"],
                                               None],  # this is hacky and usually not recommended (for ITI Jitter)
                                    timing=self.settings["session"]["timing"])
-                self.start_experiment()
                 for trial in self.trials:
+                    # make sure the default is black line color
+                    self.send_trigger("trial_onset")
+                    trigger_name = f'Target-{int(trial.parameters["TargetLoc"])}-Singleton-{int(trial.parameters["SingletonLoc"])}-{PRIMING[trial.parameters["Priming"]]}'
+                    self.send_trigger(trigger_name)
                     trial.run()
+                    self.send_trigger("trial_offset")
                 self.save_data()
                 self.plot_frame_intervals()
+                self.send_trigger("block_offset")
                 if not block == max(self.blocks):
                     self.display_text(text=prompts.pause, keys="space")
         self.display_text(text=prompts.end, keys="q")
+        self.send_trigger("experiment_offset")
+
+    # Function to send trigger value by specifying event name
+    def send_trigger(self, event_name):
+        if self.settings["mode"]["record_eeg"]:
+            # get corresponding trigger value:
+            trigger_value = EEG_TRIGGER_MAP[event_name]
+            # send trigger to EEG:
+            parallel.setData(trigger_value)
+            core.wait(0.003)  # you need a break between the triggers: wait for 3 ms
+            # turn off EEG trigger
+            parallel.setData(0)
+        else:
+            pass
 
 
 if __name__ == '__main__':
     import psychopy
     # DEBUGGING
-    sess = SpaceprimeSession(output_str='sub-999', output_dir="SPACEPRIME/logs", settings_file="SPACEPRIME/config.yaml")
+    sess = SpaceprimeSession(output_str='sub-99', output_dir="SPACEPRIME/logs", settings_file="SPACEPRIME/config.yaml")
     # sess.display_text(text=prompts.testing, keys="space")
     sess.set_block(block=1)  # intentionally choose block within
     sess.load_sequence()
