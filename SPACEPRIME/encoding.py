40f5d35d0ec8dac4a4a145a852007507a6535d35
Max <schulz.max5@gmail.com>
2024-09-20 14:52:41 +0200

    EEG trigger map exists now

--- SPACEPRIME/encoding.py
+++ SPACEPRIME/encoding.py
@@ -17,9 +17,44 @@ RESPONSE_ENCODER = {
 }
 
 EEG_TRIGGER_MAP = {
-    "block_onset": 1,
-    "start_experiment": 2,
-    "end_experiment": 3,
-    "response": 4,
-    "lol": 5
+    "test_trigger": 1,
+    "block_onset": 2,
+    "block_offset": 3,
+    "experiment_onset": 4,
+    "experiment_offset": 5,
+    "trial_onset": 6,
+    "trial_offset": 7,
+    "Target-1-Singleton-0-C": 10,
+    "Target-1-Singleton-2-C": 12,
+    "Target-1-Singleton-3-C": 13,
+    "Target-1-Singleton-0-PP": 110,
+    "Target-1-Singleton-2-PP": 112,
+    "Target-1Singleton-3-PP": 113,
+    "Target-1-Singleton-0-NP": 210,
+    "Target-1-Singleton-2-NP": 212,
+    "Target-1-Singleton-3-NP": 213,
+    "Target-2-Singleton-0-C": 20,
+    "Target-2-Singleton-1-C": 21,
+    "Target-2-Singleton-3-C": 23,
+    "Target-2-Singleton-0-PP": 120,
+    "Target-2-Singleton-1-PP": 121,
+    "Target-2-Singleton-3-PP": 123,
+    "Target-2-Singleton-0-NP": 220,
+    "Target-2-Singleton-1-NP": 221,
+    "Target-2-Singleton-3-NP": 223,
+    "Target-3-Singleton-0-C": 30,
+    "Target-3-Singleton-1-C": 31,
+    "Target-3-Singleton-2-C": 32,
+    "Target-3-Singleton-0-PP": 130,
+    "Target-3-Singleton-1-PP": 131,
+    "Target-3-Singleton-2-PP": 132,
+    "Target-3-Singleton-0-NP": 230,
+    "Target-3-Singleton-1-NP": 231,
+    "Target-3-Singleton-2-NP": 232,
+}
+
+PRIMING = {
+    0: "C",
+    1: "PP",
+    -1: "NP"
 }
