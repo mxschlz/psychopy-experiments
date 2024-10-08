ae0d1e0f1b2caff11477b794ad59d3e1de177ec4
mxschlz <schulz.max5@gmail.com>
2024-10-08 20:24:03 +0200

    Obsolete

--- check_audio_hardware.py
+++
@@ -1,7 +0,0 @@
-from psychopy import prefs
-prefs.hardware['audioLib'] = ['ptb']
-prefs.hardware['audioDevice'] = 'Lautsprecher (RME Fireface UC)'  # Replace with your device name
-
-from psychopy import sound
-
-print(sound.Sound)
