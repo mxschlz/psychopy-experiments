9bea0b03dfea992c423034c6d5c6b309f1d42d2b
Max <schulz.max5@gmail.com>
2024-11-29 16:07:41 +0100

    Plot timing delay distribution

--- SPACEPRIME/plot_timing_delay.py
+++ SPACEPRIME/plot_timing_delay.py
@@ -1,7 +1,22 @@
 import pandas as pd
-import seaborn
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+plt.ion()
 
 
-df = pd.read_csv("C:\\Users\AC_STIM\Desktop\\spaceprime_delay.csv", delimiter="\t")
-trigger_events = df[df["Keypad 1"] == 1]
-sound_events = df[df["Mic 1 "] == 1]
+# load up datafrane
+df = pd.read_csv("G:\Meine Ablage\PhD\data\BBTKv2\SPACEPRIME\\spaceprime_delay.csv", delimiter="\t")
+# get only the relevant columns
+df = df[["Mic 1", "Keypad 1", "Elapsed uS"]]
+# transform from microseconds to milliseconds
+df["Elapsed mS"] = df["Elapsed uS"] / 1000
+# delete irrelevant column
+df.pop("Elapsed uS")
+# get the relevant indices where events occur simultaneously
+indices = df[(df['Mic 1'] == 1) & (df['Keypad 1'] == 1)].index
+# subtract the previous row from the row where both events occur simultaneously
+onset_asynchrony = df.loc[indices, "Elapsed mS"].reset_index(drop=True) - df.loc[indices-1, "Elapsed mS"].reset_index(drop=True)
+# plot the stuff
+sns.displot(onset_asynchrony[onset_asynchrony < 2000])
+sns.boxplot(onset_asynchrony[onset_asynchrony < 2000])
