5c1234d1735b666f66f4853771e196f1bf136936
mxschlz <schulz.max5@gmail.com>
2024-12-02 21:44:01 +0100

    Raw spaceprime logic test

--- SPACEPRIME/test_run_spaceprime_logic.py
+++ SPACEPRIME/test_run_spaceprime_logic.py
@@ -5,24 +5,5 @@ from SPACEPRIME.experiment_logic import SpaceprimeSession
 sess = SpaceprimeSession(output_str=f'sub-105', output_dir="logs",
                          settings_file="config.yaml",
                          starting_block=0, test=True)
-#sess.win.close()
-# set block
-sess.set_block(block=1)
-# load up trial sequence
-sess.load_sequence()
-# create trials from sequence
-sess.create_trials(n_trials=5,
-                   durations=(sess.settings["session"]["stimulus_duration"],
-                              sess.settings["session"]["response_duration"],
-                              None),  # this is hacky and usually not recommended (for ITI Jitter)
-                   timing=sess.settings["session"]["timing"])
-# set up timer etc. for the experiment
-sess.start_experiment()
-# run through trials
-for trial in sess.trials:
-    trial.trigger_name = "test_trigger"
-    # play sound
-    trial.run()
-# clean up
-sess.close()
-sess.quit()
+# run testing function
+sess.bbtkv2_test_run(n_trials=50)
