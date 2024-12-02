c03c0ee36ea24583edf9bf9877283d6c3b2e171c
mxschlz <schulz.max5@gmail.com>
2024-12-02 21:43:44 +0100

    Play raw sound object with time delay

--- SPACEPRIME/test_run_raw.py
+++ SPACEPRIME/test_run_raw.py
@@ -1,20 +1,20 @@
 from psychopy import parallel
+import os
+os.environ["SD_ENABLE_ASIO"] = "1"
 from SPACEPRIME.sound import SoundDeviceSound
 import time
-import os
 
 
 port = parallel.ParallelPort(0xCFF8)
 sound_dir = 'C:\\Users\AC_STIM\Documents\Experimentskripte\max_schulz_scripts\psychopy-experiments\SPACEPRIME\sequences\sub-102_block_0'
-sounds = []
-for sound_path in os.listdir(sound_dir):
-    sound = SoundDeviceSound(os.path.join(sound_dir, sound_path), device=44, mul=25)
-    sounds.append(sound)
+sounds = [os.path.join(sound_dir, os.listdir(sound_dir)[x]) for x in range(len(os.listdir(sound_dir)))]
 
-for i in range(15):
-    sounds[i].play()
+for i, _ in enumerate(range(30)):
+    print(f"Playing sound {i}")
+    sound = SoundDeviceSound(sounds[i], device=44, mul=25)
+    sound.play(latency=0.0, blocksize=0)
     time.sleep(0.08)
     port.setData(1)
     time.sleep(0.001)
     port.setData(0)
-    time.sleep(1)
+    time.sleep(0.5)
