a6d6160a1020f814c7e74f6ff9c6fe69674d80bb
Max <schulz.max5@gmail.com>
2024-12-04 17:48:17 +0100

    Transfer behavioral data to cloud

--- SPACEPRIME/transfer_data_to_cloud.py
+++ SPACEPRIME/transfer_data_to_cloud.py
@@ -1,11 +1,35 @@
 import os
 from utils.utils import get_input_from_dict
+import shutil
 
 
+# define subject id
 info = get_input_from_dict({"subject_id": 99})
-
+# TRANSFER CLEAN BEHAVIOR
+print("Copying clean data ...")
 # Create the destination directory if it doesn't exist
-destination_dir = f'G:\\Meine Ablage\\PhD\\data\\SPACEPRIME\\derivatives\\preprocessing\\sub-{info["subject_id"]}\\beh'
-os.makedirs(destination_dir, exist_ok=True)
+destination_dir_clean = f'G:\\Meine Ablage\\PhD\\data\\SPACEPRIME\\derivatives\\preprocessing\\sub-{info["subject_id"]}\\beh'
+print(f"Creating diriectory {destination_dir_clean}")
+os.makedirs(destination_dir_clean, exist_ok=True)  # create directory no matter if it already exists
+# get source dir
+source_dir_clean = "logs\\clean"
 # get source path
-source_path = f'logs\clean\sub-{info["subject_id"]}_clean.csv'
+source_path_clean = os.path.join(source_dir_clean, f'sub-{info["subject_id"]}_clean.csv')
+destination_path_clean = os.path.join(destination_dir_clean, f'sub-{info["subject_id"]}_clean.csv')
+print(f"Transferring file {source_path_clean} to {destination_path_clean}")
+shutil.copy(source_path_clean, destination_path_clean)
+# TRANSFER RAW BEHAVIOR
+print("Copying raw data ...")
+# Create the destination directory if it doesn't exist
+destination_dir_raw = f'G:\\Meine Ablage\\PhD\\data\\SPACEPRIME\\sourcedata\\raw\\sub-{info["subject_id"]}\\beh'
+print(f"Creating directory {destination_dir_raw}")
+os.makedirs(destination_dir_raw, exist_ok=True)  # create directory no matter if it already exists
+# get source dir
+source_dir_raw = "logs"
+# move all files from that directory to destination
+for file in os.listdir(source_dir_raw):
+	source_path_raw = os.path.join(source_dir_raw, file)
+	destination_path_raw = os.path.join(destination_dir_raw, file)
+	if os.path.isfile(source_path_raw):
+		print(f"Transferring {source_path_raw} to {destination_path_raw}")
+		shutil.copy(source_path_raw, destination_path_raw)
