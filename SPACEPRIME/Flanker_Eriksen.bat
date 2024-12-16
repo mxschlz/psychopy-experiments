a30b02024a7c992495de2b2e5994bdb76dc15803
Max <schulz.max5@gmail.com>
2024-12-16 14:16:34 +0100

    Run flanker task

---
+++ SPACEPRIME/Flanker_Eriksen.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+
+:: Replace with the actual name of your Anaconda environment
+SET CONDA_ENV=psypy
+
+:: Replace with the actual path to your project directory
+SET PROJECT_DIR=C:\Users\AC_STIM\Documents\Experimentskripte\max_schulz_scripts\psychopy-experiments\SPACEPRIME
+
+:: Replace with the actual name of your Python script
+SET PYTHON_SCRIPT=Flanker_Eriksen.py
+
+:: Activate the Anaconda environment
+CALL conda activate %CONDA_ENV%
+
+:: Change to the project directory
+CD /D %PROJECT_DIR%
+
+:: Execute the Python script
+python %PYTHON_SCRIPT%
+
+:: Deactivate the environment if you want (optional)
+CALL conda deactivate
