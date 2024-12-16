6e8e4e203157bad50e90519fcdd7ef4316d4176f
Max <schulz.max5@gmail.com>
2024-12-16 14:02:28 +0100

    Finalized prompts

--- SPACEPRIME/prompts.py
+++ SPACEPRIME/prompts.py
@@ -1,80 +1,81 @@
 prompt1 = '''
-Willkommen und vielen Dank für die Teilnahme am Experiment zur räumlichen Aufmerksamkeit.
-Im Verlauf des Experiments sollst du eine Zielziffer aus jeweils drei verschiedenen Ziffern identifizieren.
-Versuche dabei stets, so schnell wie möglich zu antworten! Das Experiment ist in 10 Blöcke aufgeteilt, zwischen denen
-du kurze Pausen einlegen kannst.
+Willkommen und vielen Dank für die Teilnahme an diesem Experiment zur räumlichen Aufmerksamkeit. In jedem Durchgang 
+des Experiments sollen Sie ein Zahlwort aus jeweils drei verschiedenen Zahlwörtern identifizieren. Versuchen Sie dabei 
+stets, so korrekt und so schnell wie möglich zu antworten! Das Experiment ist in 10 Blöcke aufgeteilt, zwischen denen 
+Sie kurze Pausen einlegen können.
 
-Drücke LEERTASTE, um weiterzublättern.
+Drücken Sie LEERTASTE, um weiterzublättern.
 '''
 
 prompt2 = """
-In jedem Durchgang des Experiments erscheint gleichzeitig jeweils eine Ziffer aus einem der drei Lautsprecher, die dich
-umgeben. Bei den Ziffern handelt es sich immer um ein und die selbe Person, welche Zahlen zwischen 1 und 9 ausspricht.
-In einem Durchgang sind alle Ziffern einzigartig, z.B. kann die Ziffer 9 nicht sowohl aus dem linken, als auch dem
-rechten Lautsprecher gleichzeitig ertönen.
+In jedem Durchgang des Experiments werden gleichzeitig drei Zahlwörter von drei Lautsprechern abgespielt. Alle 
+Zahlwörter werden von derselben Stimme gesprochen. Bei den Zahlwörtern handelt es sich um eine Auswahl der Zahlen 
+zwischen 1 und 9. In einem Durchgang sind alle Ziffern einzigartig, z.B. kann die Zahl 9 nicht sowohl aus dem linken 
+als auch dem rechten Lautsprecher gleichzeitig ertönen.
 
-Drücke LEERTASTE, um weiterzublättern.
+Drücken Sie LEERTASTE, um weiterzublättern.
 """
 
 prompt3 = """
-Eine Ziffer unterscheidet sich von den anderen beiden in einer Eigenschaft: Sie wird im Zuge der Artikulation lauter und
-leiser und klingt deshalb etwas eigenartig. Diese Ziffer ist wie ein Licht, das in schneller Abfolge heller und dunkler
-wird. Auf genau diese Ziffer sollst du dich stets konzentrieren und ihre Identität so schnell wie möglich angeben!
+Das Zahlwort, welches Sie identifizieren sollen, unterscheidet sich von den anderen beiden in einer Eigenschaft: Dieses 
+Zahlwort schwankt schnell in seiner Lautstärke und wird im Zuge der Artikulation abwechselnd lauter und leiser. Auf 
+genau dieses Zahlwort sollen Sie sich konzentrieren und die Zahl so schnell wie möglich angeben!
 
-Drücke LEERTASTE, um weiterzublättern.
+Drücken Sie LEERTASTE, um weiterzublättern.
 """
 
 prompt4 = """
-In manchen Durchgängen ist eine der zwei weiteren Ziffern ebenfalls abweichend. Diese besitzt dann eine andere
-Tonhöhe. Lass dich davon nicht irritieren: deine Aufgabe bleibt es, stets die Zielziffer zu benennen!
+In manchen Durchgängen hat eines der zwei weiteren Zahlwörter eine andere Tonhöhe. Lassen Sie sich davon nicht 
+irritieren: Ihre Aufgabe bleibt es, stets das Zahlwort zu benennen, welches in seiner Lautstärke schnell schwankt.
 
-Drücke LEERTASTE, weiterzublättern.
+Drücken Sie LEERTASTE, um weiterzublättern.
 """
 
 demo = """
-Im Kommenden wirst du einen Eindruck davon erhalten, wie sich die Zielziffern anhören. Es werden langsam nacheinander
-alle Ziffern von 1 bis 9 ein Mal abgespielt.
+Im Kommenden werden Sie einen Eindruck davon erhalten, wie sich die Zahlwörter anhören. Jedes Zahlwort wird zunächst 
+normal abgespielt, gefolgt von einer Darbietung mit schnellen Schwankungen der Lautstärke.
 
-Drücke LEERTASTE, um die Zielziffern zwischen 1 und 9 anzuhören.
+Drücken Sie LEERTASTE, um die Zielziffern zwischen 1 und 9 anzuhören.
 """
 
 prompt5 = """
-Während des Experiments erscheint eine Antwort-Box in der Mitte des Bildschirms. Diese enthält die Ziffern von 1 bis 9
-und ist durch einen schwarzen Rahmen begrenzt. Mit der Maus kannst du auf eine Ziffer pro Durchgang klicken, um die
-Zielziffer zu identifizieren. Solltest du zu langsam antworten, färbt sich der Rahmen der Box für eine kurze Zeit rot.
-Das ist dann der Hinweis, dass du in den kommenden Durchgängen etwas schneller antworten solltest.
+Während des Experiments erscheint eine Antwort-Box in der Mitte des Bildschirms. Diese enthält die Ziffern von 1 bis 9 
+und ist durch einen schwarzen Rahmen begrenzt. Mit der Maus können Sie auf eine Zahl pro Durchgang klicken, um das 
+Zahlwort anzugeben, welches in seiner Lautstärke geschwankt hat. 
 
-Drücke LEERTASTE, um weiterzublättern.
+Drücken Sie LEERTASTE, um weiterzublättern.
 """
 
 prompt6 = """
-Bitte halte deinen Blick während der Aufgabe stets auf die Antwort-Box gerichtet. Die Ziffern färben sich dunkel, wenn
-du den Mauszeiger über sie bewegst. Das ist dann das Zeichen, dass du die jeweilige Ziffer bestätigen kannst.
-Der Mauszeiger erscheint in der Mitte der Box am Anfang jedes Durchgangs und verschwindet am Ende.
+Sollten Sie zu langsam antworten, färbt sich der Rahmen der Box für eine kurze Zeit rot. Das ist dann der Hinweis, dass 
+Sie in den kommenden Durchgängen etwas schneller antworten sollen. Bitte halten Sie Ihren Blick während der Aufgabe 
+stets auf die Antwort-Box gerichtet. Die Zahlen färben sich dunkel, wenn Sie den Mauszeiger über sie bewegen. Der 
+Mauszeiger erscheint in der Mitte der Box am Anfang jedes Durchgangs und verschwindet, sobald Sie eine Zahl ausgewählt 
+haben.
 
-Drücke LEERTASTE, um weiterzublättern.
+Drücken Sie LEERTASTE, um weiterzublättern.
 """
 
 testing = """
-Im kommenden werden dir einige Probe-Durchläufe präsentiert. Sie sollen dich mit der Aufgabe vertraut machen.
-Du kannst üben und Antworten geben, diese werden natürlich nicht gespeichert. Bitte nutze diese Phase, um so gut wie
-möglich mit dem Experiment vertraut zu werden. Nach diesem Testblock kannst Fragen an die Versuchsleitung stellen oder
-Probleme benennen, falls etwas nicht stimmen sollte. Nach diesem Testblock geht es mit der Hauptaufgabe los.
+Im kommenden werden Ihnen einige Probe-Durchläufe präsentiert. Diese sollen Sie mit der Aufgabe vertraut machen. Sie 
+können üben und Antworten geben, diese werden natürlich nicht gespeichert. Bitte nutzen Sie diese Phase, um so gut wie 
+möglich mit dem Experiment vertraut zu werden. Nach diesem Testblock können Sie Fragen an die Versuchsleitung stellen. 
+Nach diesem Testblock startet das Hauptexperiment.
 
-Drücke LEERTASTE, um zu beginnen.
+Drücken Sie LEERTASTE, um zu beginnen.
 """
 
 camera_calibration = """
-Bitte schaue für die nächsten 10 Sekunden auf den Fixationspunkt. Achte darauf, dass dein Kopf und deine Augen geradeaus
-auf den Punkt gerichtet sind. Danach geht es mit dem Experiment weiter.
+Bitte schauen Sie für die nächsten 10 Sekunden auf den Fixationspunkt. Achten Sie darauf, dass Ihr Kopf und Ihre Augen 
+geradeaus auf den Punkt gerichtet sind. Danach geht es mit dem Experiment weiter.
 
-Drücke LEERTASTE, um zu beginnen.
+Drücken Sie LEERTASTE, um zu beginnen.
 """
 
 pause = """
-Der Block ist vorbei. Bitte nimm Dir eine Pause. Sobald Du weitermachen möchtest, drücke LEERTASTE.
+Dieser Block ist zu Ende. Bitte machen Sie eine kurze Pause. Sobald Sie weitermachen möchten, drücken Sie LEERTASTE.
 """
 
 end = """
-Das Experiment ist vorbei. Vielen Dank für die Teilnahme! Du kannst nun die Versuchsleitung kontaktieren.
+Das Experiment ist nun vorbei. Vielen Dank für die Teilnahme! Sie können sich nun an die Versuchsleitung wenden.
 """
