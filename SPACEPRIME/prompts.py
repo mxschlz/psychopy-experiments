9a1e814647c66b43a5b1295588082c75bf8d1ad5
mxschlz <schulz.max5@gmail.com>
2025-01-07 16:11:21 +0100

    Adjusted instruction text format

--- SPACEPRIME/prompts.py
+++ SPACEPRIME/prompts.py
@@ -1,76 +1,78 @@
 prompt1 = '''
-Willkommen und vielen Dank für die Teilnahme an diesem Experiment zur räumlichen Aufmerksamkeit. In jedem Durchgang 
-des Experiments sollen Sie ein Zahlwort aus jeweils drei verschiedenen Zahlwörtern identifizieren. Versuchen Sie dabei 
-stets, so korrekt und so schnell wie möglich zu antworten! Das Experiment ist in 10 Blöcke aufgeteilt, zwischen denen 
-Sie kurze Pausen einlegen können.
+Willkommen und vielen Dank für die Teilnahme an diesem Experiment zur räumlichen Aufmerksamkeit. \n
+In jedem Durchgang des Experiments sollen Sie ein Zahlwort aus jeweils drei verschiedenen Zahlwörtern identifizieren. \n
+Versuchen Sie dabei stets, so korrekt und so schnell wie möglich zu antworten! \n
+Das Experiment ist in 10 Blöcke aufgeteilt, zwischen denen Sie kurze Pausen einlegen können. \n
 
 Drücken Sie LEERTASTE, um weiterzublättern.
 '''
 
 prompt2 = """
-In jedem Durchgang des Experiments werden gleichzeitig drei Zahlwörter von drei Lautsprechern abgespielt. Alle 
-Zahlwörter werden von derselben Stimme gesprochen. Bei den Zahlwörtern handelt es sich um eine Auswahl der Zahlen 
-zwischen 1 und 9. In einem Durchgang sind alle Ziffern einzigartig, z.B. kann die Zahl 9 nicht sowohl aus dem linken 
-als auch dem rechten Lautsprecher gleichzeitig ertönen.
+In jedem Durchgang des Experiments werden gleichzeitig drei Zahlwörter von drei Lautsprechern abgespielt. \n
+Alle Zahlwörter werden von derselben Stimme gesprochen. Bei den Zahlwörtern handelt es sich um eine Auswahl der Zahlen 
+zwischen 1 und 9. \n
+In einem Durchgang sind alle Ziffern einzigartig, z.B. kann die Zahl 9 nicht sowohl aus dem linken als auch dem rechten Lautsprecher gleichzeitig ertönen. \n
 
 Drücken Sie LEERTASTE, um weiterzublättern.
 """
 
 prompt3 = """
-Das Zahlwort, welches Sie identifizieren sollen, unterscheidet sich von den anderen beiden in einer Eigenschaft: 
-Dieses Zahlwort klingt im Vergleich zu den restlichen Zahlwörtern sehr rau und kratzig. Die Stimme dieses Wortes klingt 
-wie in einer schlechten Telefonverbindung. Auf genau dieses Zahlwort sollen Sie sich konzentrieren und die Zahl so 
-schnell wie möglich angeben!
+Das Zahlwort, welches Sie identifizieren sollen, unterscheidet sich von den anderen beiden in einer Eigenschaft: \n
+Dieses Zahlwort klingt im Vergleich zu den restlichen Zahlwörtern sehr rau und kratzig. \n
+Die Stimme dieses Wortes klingt wie in einer schlechten Telefonverbindung. \n
+Auf genau dieses Zahlwort sollen Sie sich konzentrieren und die Zahl so schnell wie möglich angeben! \n
 
 Drücken Sie LEERTASTE, um weiterzublättern.
 """
 
 prompt4 = """
-In manchen Durchgängen hat eines der zwei weiteren Zahlwörter eine andere Tonhöhe. Lassen Sie sich davon nicht 
-irritieren: Ihre Aufgabe bleibt es, stets das Zahlwort zu benennen, welches in seiner Lautstärke schnell schwankt.
+In manchen Durchgängen hat eines der zwei weiteren Zahlwörter eine andere Tonhöhe. \n
+Lassen Sie sich davon nicht irritieren: Ihre Aufgabe bleibt es, stets das Zahlwort zu benennen, welches in seiner Lautstärke schnell schwankt. \n
 
 Drücken Sie LEERTASTE, um weiterzublättern.
 """
 
 demo = """
-Im Kommenden werden Sie einen Eindruck davon erhalten, wie sich die Zahlwörter anhören. Es werden nur die relevanten
-Zahlwörter abgespielt, auf die Sie achten sollen. Dabei ertönen sie zufällig aus einem der drei Lautsprecher, so wie 
-es auch im Hauptexperiment sein wird. ACHTUNG: erschrecken Sie bitte nicht.
+Im Kommenden werden Sie einen Eindruck davon erhalten, wie sich die Zahlwörter anhören. \n
+Es werden nur die relevanten Zahlwörter abgespielt, auf die Sie achten sollen. \n
+Dabei ertönen sie zufällig aus einem der drei Lautsprecher, so wie es auch im Hauptexperiment sein wird. \n
+ACHTUNG: erschrecken Sie bitte nicht. \n
 
 Drücken Sie LEERTASTE, um die Zielziffern zwischen 1 und 9 anzuhören.
 """
 
 prompt5 = """
-Während des Experiments erscheint eine Antwort-Box in der Mitte des Bildschirms. Diese enthält die Ziffern von 1 bis 9 
-und ist durch einen schwarzen Rahmen begrenzt. Mit der Maus können Sie auf eine Zahl pro Durchgang klicken, um das 
-Zahlwort anzugeben, welches in seiner Lautstärke geschwankt hat. ACHTUNG: die Antwort ist nur gültig, wenn der 
-Mauszeiger nach dem Klicken verschwindet! 
+Während des Experiments erscheint eine Antwort-Box in der Mitte des Bildschirms. \n
+Diese enthält die Ziffern von 1 bis 9 und ist durch einen schwarzen Rahmen begrenzt. \n
+Mit der Maus können Sie auf eine Zahl pro Durchgang klicken, um das Zahlwort anzugeben, welches in seiner Lautstärke geschwankt hat. \n
+ACHTUNG: die Antwort ist nur gültig, wenn der Mauszeiger nach dem Klicken verschwindet! \n
 
 Drücken Sie LEERTASTE, um weiterzublättern.
 """
 
 prompt6 = """
-Sollten Sie zu langsam antworten, färbt sich der Rahmen der Box für eine kurze Zeit rot. Das ist dann der Hinweis, dass 
-Sie in den kommenden Durchgängen etwas schneller antworten sollen. Bitte halten Sie Ihren Blick während der Aufgabe 
-stets auf die Antwort-Box gerichtet. Die Zahlen färben sich dunkel, wenn Sie den Mauszeiger über sie bewegen. Der 
-Mauszeiger erscheint in der Mitte der Box am Anfang jedes Durchgangs und verschwindet, sobald Sie eine Zahl ausgewählt 
-haben.
+Sollten Sie zu langsam antworten, färbt sich der Rahmen der Box für eine kurze Zeit rot. \n
+Das ist dann der Hinweis, dass Sie in den kommenden Durchgängen etwas schneller antworten sollen. \n
+Bitte halten Sie Ihren Blick während der Aufgabe stets auf die Antwort-Box gerichtet. \n
+Die Zahlen färben sich dunkel, wenn Sie den Mauszeiger über sie bewegen. \n
+Der Mauszeiger erscheint in der Mitte der Box am Anfang jedes Durchgangs und verschwindet, sobald Sie eine Zahl ausgewählt haben. \n
 
 Drücken Sie LEERTASTE, um weiterzublättern.
 """
 
 testing = """
-Im Kommenden werden Ihnen einige Probe-Durchläufe präsentiert. Diese sollen Sie mit der Aufgabe vertraut machen. Sie 
-können üben und Antworten geben, diese werden natürlich nicht gespeichert. Bitte nutzen Sie diese Phase, um so gut wie 
-möglich mit dem Experiment vertraut zu werden. Nach diesem Testblock können Sie Fragen an die Versuchsleitung stellen. 
-Nach diesem Testblock startet das Hauptexperiment.
+Im Kommenden werden Ihnen einige Probe-Durchläufe präsentiert. Diese sollen Sie mit der Aufgabe vertraut machen. \n
+Sie können üben und Antworten geben, diese werden natürlich nicht gespeichert. \n
+Bitte nutzen Sie diese Phase, um so gut wie möglich mit dem Experiment vertraut zu werden. \n
+Nach diesem Testblock können Sie Fragen an die Versuchsleitung stellen. Nach diesem Testblock startet das Hauptexperiment. \n
 
 Drücken Sie LEERTASTE, um zu beginnen.
 """
 
 camera_calibration = """
-Bitte schauen Sie für die nächsten 10 Sekunden auf den Fixationspunkt. Achten Sie darauf, dass Ihr Kopf und Ihre Augen 
-geradeaus auf den Punkt gerichtet sind. Danach geht es mit dem Experiment weiter.
+Bitte schauen Sie für die nächsten 10 Sekunden auf den Fixationspunkt. \n
+Achten Sie darauf, dass Ihr Kopf und Ihre Augen geradeaus auf den Punkt gerichtet sind. \n
+Danach geht es mit dem Experiment weiter. \n
 
 Drücken Sie LEERTASTE, um zu beginnen.
 """
@@ -84,5 +86,6 @@ Die Pause ist zu Ende. Drücken Sie LEERTASTE, um den nächsten Block zu beginne
 """
 
 end = """
-Das Experiment ist nun vorbei. Vielen Dank für die Teilnahme! Sie können sich nun an die Versuchsleitung wenden.
+Das Experiment ist nun vorbei. Vielen Dank für die Teilnahme! \n
+Sie können sich nun an die Versuchsleitung wenden.
 """
