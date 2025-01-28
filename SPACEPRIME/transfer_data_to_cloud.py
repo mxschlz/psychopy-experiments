074c375d8d4c0f40b816110707c0333aaf54f6af
mxschlz <schulz.max5@gmail.com>
2025-01-28 17:21:19 +0100

    transfer questionnaire data transfer trial sequences

--- SPACEPRIME/transfer_data_to_cloud.py
+++ SPACEPRIME/transfer_data_to_cloud.py
@@ -39,18 +39,59 @@ for file in os.listdir(source_dir_raw):
 	if os.path.isfile(source_path_raw):
 		print(f"Transferring {source_path_raw} to {destination_path_raw}")
 		shutil.copy(source_path_raw, destination_path_raw)
-# TRANSFER FLANKER DATA
-print("Copying flanker data ...")
+try:
+	# TRANSFER FLANKER BEHAVIOR
+	print("Copying flanker data ...")
+	# Create the destination directory if it doesn't exist
+	destination_dir_flanker = f'G:\\Meine Ablage\\PhD\\data\\SPACEPRIME\\sourcedata\\raw\\sub-{info["subject_id"]}\\beh'
+	print(f"Creating directory {destination_dir_flanker}")
+	os.makedirs(destination_dir_flanker, exist_ok=True)  # create directory no matter if it already exists
+	# get source dir
+	source_dir_flanker = "logs"
+	# get source path
+	glob_file_flanker = os.path.join(source_dir_flanker, f'flanker_data_{info["subject_id"]}*.csv')
+	glob_file_clean_flanker = glob.glob(glob_file_flanker)[0].split("\\")[1]
+	destination_path_clean_flanker = os.path.join(destination_dir_flanker, glob_file_clean_flanker)
+	filename_clean_flanker = os.path.join(source_dir_flanker, glob_file_clean_flanker)
+	print(f"Transferring file {filename_clean_flanker} to {destination_path_clean_flanker}")
+	shutil.copy(filename_clean_flanker, destination_path_clean_flanker)
+except IndexError:
+	print(f"No flanker data found for subject {info['subject_id']}.")
+try:
+	# TRANSFER QUESTIONNAIRES
+	print("Copying questionnaire data ...")
+	# Create the destination directory if it doesn't exist
+	destination_dir_questionnaire = f'G:\\Meine Ablage\\PhD\\data\\SPACEPRIME\\sourcedata\\raw\\sub-{info["subject_id"]}\\questionnaires'
+	print(f"Creating directory {destination_dir_questionnaire}")
+	os.makedirs(destination_dir_questionnaire, exist_ok=True)  # create directory no matter if it already exists
+	# get source dir
+	source_dir_questionnaire = "logs\\questionnaires"
+	# get source path
+	glob_file_questionnaire = os.path.join(source_dir_questionnaire, f'*{info["subject_id"]}*.csv')
+	for i in range(2):  # 2 questionnaires
+		glob_file_clean_questionnaire = glob.glob(glob_file_questionnaire)[i].split("\\")[2]
+		destination_path_clean_questionnaire = os.path.join(destination_dir_questionnaire, glob_file_clean_questionnaire)
+		filename_clean_questionnaire = os.path.join(source_dir_questionnaire, glob_file_clean_questionnaire)
+		print(f"Transferring file {filename_clean_questionnaire} to {destination_path_clean_questionnaire}")
+		shutil.copy(filename_clean_questionnaire, destination_path_clean_questionnaire)
+except IndexError:
+	print(f"No questionnaire data found for subject {info['subject_id']}.")
+# TRANSFER TRIAL SEQUENCES
+print("Copying trial sequences ...")
 # Create the destination directory if it doesn't exist
-destination_dir_flanker = f'G:\\Meine Ablage\\PhD\\data\\SPACEPRIME\\sourcedata\\raw\\sub-{info["subject_id"]}\\beh'
-print(f"Creating directory {destination_dir_flanker}")
-os.makedirs(destination_dir_flanker, exist_ok=True)  # create directory no matter if it already exists
+destination_dir_sequences = f'G:\\Meine Ablage\\PhD\\data\\SPACEPRIME\\sequences\\sub-{info["subject_id"]}'
+print(f"Creating directory {destination_dir_sequences}")
+os.makedirs(destination_dir_sequences, exist_ok=True)  # create directory no matter if it already exists
 # get source dir
-source_dir_flanker = "logs"
+source_dir_sequences = "sequences"
 # get source path
-glob_file_flanker = os.path.join(source_dir_flanker, f'flanker_data_{info["subject_id"]}_*.csv')
-glob_file_clean_flanker = glob.glob(glob_file_flanker)[0].split("\\")[1]
-destination_path_clean_flanker = os.path.join(destination_dir_flanker, glob_file_clean_flanker)
-filename_clean_flanker = os.path.join(source_dir_flanker, glob_file_clean_flanker)
-print(f"Transferring file {filename_clean_flanker} to {destination_path_clean_flanker}")
-shutil.copy(filename_clean_flanker, destination_path_clean_flanker)
+# move all files from that directory to destination
+for file in os.listdir(source_dir_sequences):
+	if f'sub-{info["subject_id"]}' in file:
+		source_path_sequences = os.path.join(source_dir_sequences, file)
+	else:
+		continue
+	destination_path_sequences = os.path.join(destination_dir_sequences, file)
+	if os.path.isfile(source_path_sequences):
+		print(f"Transferring {source_path_sequences} to {destination_path_sequences}")
+		shutil.copy(source_path_sequences, destination_path_sequences)
