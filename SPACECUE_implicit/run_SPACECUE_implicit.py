b8128b3d528939a343e2f8bfa38344ad22dea1d2
Max <schulz.max5@gmail.com>
2025-10-08 15:02:13 +0200

    Update

--- SPACECUE_implicit/run_SPACECUE_implicit.py
+++ SPACECUE_implicit/run_SPACECUE_implicit.py
@@ -8,7 +8,7 @@ info = get_input_from_dict({"subject_id": 99,  # enter subject id
 
 # create subject sequence
 # generate_subject_sequence(subject_id=info["subject_id"])
-sess = SpacecueImplicitSession(output_str=f'sub-{info["subject_id"]}', output_dir="logs",
+sess = SpacecueImplicitSession(output_str=f'sci-{info["subject_id"]}', output_dir="logs",
                          settings_file="config.yaml",
                          starting_block=info["block"], test=True if info["test"] == 1 else False)
 sess.run(starting_block=info["block"])
