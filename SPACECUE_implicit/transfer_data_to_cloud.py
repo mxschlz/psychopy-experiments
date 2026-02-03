1d723311e803748c0cf9c79759e5dceeae12a7de
max <schulz.max5@gmail.com>
2026-02-03 13:02:44 +0100

    Transfer data to cloud

--- SPACECUE_implicit/transfer_data_to_cloud.py
+++ SPACECUE_implicit/transfer_data_to_cloud.py
@@ -5,18 +5,21 @@ import glob
 
 
 # define subject id
-info = get_input_from_dict({"subject_id": 99})
+info = get_input_from_dict({"subject_id": 99,
+                            "isPilot": True})
+
 # TRANSFER CLEAN BEHAVIOR
 print("Copying clean data ...")
 # Create the destination directory if it doesn't exist
-destination_dir_clean = f'G:\\Meine Ablage\\PhD\\data\\SPACECUE_implicit\\derivatives\\preprocessing\\sci-{info["subject_id"]}\\beh'
+if info["isPilot"]:
+    destination_dir_clean = f'S:\\MSchulz\\Meine Ablage\\PhD\\data\\SPACECUE_implicit\\pilot\\sci-{info["subject_id"]}\\beh'
 print(f"Creating directory {destination_dir_clean}")
 os.makedirs(destination_dir_clean, exist_ok=True)  # create directory no matter if it already exists
 # get source dir
 source_dir_clean = "../SPACECUE_implicit/logs/clean"
 # get source path
 glob_file = os.path.join(source_dir_clean, f'sci-{info["subject_id"]}_clean*.csv')
-glob_file_clean = glob.glob(glob_file)[0].split("\\")[2]
+glob_file_clean = glob.glob(glob_file)[0].split("\\")[1]
 destination_path_clean = os.path.join(destination_dir_clean, glob_file_clean)
 filename_clean = os.path.join(source_dir_clean, glob_file_clean)
 print(f"Transferring file {filename_clean} to {destination_path_clean}")
@@ -24,7 +27,7 @@ shutil.copy(filename_clean, destination_path_clean)
 # TRANSFER RAW BEHAVIOR
 print("Copying raw data ...")
 # Create the destination directory if it doesn't exist
-destination_dir_raw = f'G:\\Meine Ablage\\PhD\\data\\SPACECUE_implicit\\sourcedata\\raw\\sci-{info["subject_id"]}\\beh'
+destination_dir_raw = f'S:\\MSchulz\\Meine Ablage\\PhD\\data\\SPACECUE_implicit\\sourcedata\\raw\\sci-{info["subject_id"]}\\beh'
 print(f"Creating directory {destination_dir_raw}")
 os.makedirs(destination_dir_raw, exist_ok=True)  # create directory no matter if it already exists
 # get source dir
@@ -43,43 +46,43 @@ try:
 	# TRANSFER FLANKER BEHAVIOR
 	print("Copying flanker data ...")
 	# Create the destination directory if it doesn't exist
-	destination_dir_flanker = f'G:\\Meine Ablage\\PhD\\data\\SPACECUE_implicit\\sourcedata\\raw\\sci-{info["subject_id"]}\\beh'
-	print(f"Creating directory {destination_dir_flanker}")
-	os.makedirs(destination_dir_flanker, exist_ok=True)  # create directory no matter if it already exists
+	#destination_dir_flanker = f'G:\\Meine Ablage\\PhD\\data\\SPACECUE_implicit\\sourcedata\\raw\\sci-{info["subject_id"]}\\beh'
+	#print(f"Creating directory {destination_dir_flanker}")
+	#os.makedirs(destination_dir_flanker, exist_ok=True)  # create directory no matter if it already exists
 	# get source dir
-	source_dir_flanker = "../SPACECUE_implicit/logs"
+	#source_dir_flanker = "../SPACECUE_implicit/logs"
 	# get source path
-	glob_file_flanker = os.path.join(source_dir_flanker, f'flanker_data_{info["subject_id"]}*.csv')
-	glob_file_clean_flanker = glob.glob(glob_file_flanker)[0].split("\\")[1]
-	destination_path_clean_flanker = os.path.join(destination_dir_flanker, glob_file_clean_flanker)
-	filename_clean_flanker = os.path.join(source_dir_flanker, glob_file_clean_flanker)
-	print(f"Transferring file {filename_clean_flanker} to {destination_path_clean_flanker}")
-	shutil.copy(filename_clean_flanker, destination_path_clean_flanker)
+	#glob_file_flanker = os.path.join(source_dir_flanker, f'flanker_data_{info["subject_id"]}*.csv')
+	#glob_file_clean_flanker = glob.glob(glob_file_flanker)[0].split("\\")[1]
+	#destination_path_clean_flanker = os.path.join(destination_dir_flanker, glob_file_clean_flanker)
+	#filename_clean_flanker = os.path.join(source_dir_flanker, glob_file_clean_flanker)
+	#print(f"Transferring file {filename_clean_flanker} to {destination_path_clean_flanker}")
+	#shutil.copy(filename_clean_flanker, destination_path_clean_flanker)
 except IndexError:
 	print(f"No flanker data found for subject {info['subject_id']}.")
 try:
 	# TRANSFER QUESTIONNAIRES
 	print("Copying questionnaire data ...")
 	# Create the destination directory if it doesn't exist
-	destination_dir_questionnaire = f'G:\\Meine Ablage\\PhD\\data\\SPACECUE_implicit\\sourcedata\\raw\\sci-{info["subject_id"]}\\questionnaires'
-	print(f"Creating directory {destination_dir_questionnaire}")
-	os.makedirs(destination_dir_questionnaire, exist_ok=True)  # create directory no matter if it already exists
+	#destination_dir_questionnaire = f'G:\\Meine Ablage\\PhD\\data\\SPACECUE_implicit\\sourcedata\\raw\\sci-{info["subject_id"]}\\questionnaires'
+	#print(f"Creating directory {destination_dir_questionnaire}")
+	#os.makedirs(destination_dir_questionnaire, exist_ok=True)  # create directory no matter if it already exists
 	# get source dir
-	source_dir_questionnaire = "logs\\questionnaires"
+	#source_dir_questionnaire = "logs\\questionnaires"
 	# get source path
-	glob_file_questionnaire = os.path.join(source_dir_questionnaire, f'*{info["subject_id"]}*.csv')
-	for i in range(2):  # 2 questionnaires
-		glob_file_clean_questionnaire = glob.glob(glob_file_questionnaire)[i].split("\\")[2]
-		destination_path_clean_questionnaire = os.path.join(destination_dir_questionnaire, glob_file_clean_questionnaire)
-		filename_clean_questionnaire = os.path.join(source_dir_questionnaire, glob_file_clean_questionnaire)
-		print(f"Transferring file {filename_clean_questionnaire} to {destination_path_clean_questionnaire}")
-		shutil.copy(filename_clean_questionnaire, destination_path_clean_questionnaire)
+	#glob_file_questionnaire = os.path.join(source_dir_questionnaire, f'*{info["subject_id"]}*.csv')
+	#for i in range(2):  # 2 questionnaires
+		#glob_file_clean_questionnaire = glob.glob(glob_file_questionnaire)[i].split("\\")[2]
+		#destination_path_clean_questionnaire = os.path.join(destination_dir_questionnaire, glob_file_clean_questionnaire)
+		#filename_clean_questionnaire = os.path.join(source_dir_questionnaire, glob_file_clean_questionnaire)
+		#print(f"Transferring file {filename_clean_questionnaire} to {destination_path_clean_questionnaire}")
+		#shutil.copy(filename_clean_questionnaire, destination_path_clean_questionnaire)
 except IndexError:
 	print(f"No questionnaire data found for subject {info['subject_id']}.")
 # TRANSFER TRIAL SEQUENCES
 print("Copying trial sequences ...")
 # Create the destination directory if it doesn't exist
-destination_dir_sequences = f'G:\\Meine Ablage\\PhD\\data\\SPACECUE_implicit\\sequences\\sci-{info["subject_id"]}'
+destination_dir_sequences = f'S:\\MSchulz\\Meine Ablage\\PhD\\data\\SPACECUE_implicit\\sequences\\sci-{info["subject_id"]}'
 print(f"Creating directory {destination_dir_sequences}")
 os.makedirs(destination_dir_sequences, exist_ok=True)  # create directory no matter if it already exists
 # get source dir
